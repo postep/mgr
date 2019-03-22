6a490532136cacccbf19a04910894dd72a942ccf
postep <jakub.postepski@gmail.com>
2019-03-22 09:45:25 +0100

    x

--- sim1.m
+++ sim1.m
@@ -1,14 +1,14 @@
 clear variables;
-f = 1;
+f = 100;
 T = 1:20*f;
 b = 1;
-k = 3;
-m = 10;
+k = 100;
+m = 3;
 g = -9.81;
 
 
-a = 5;
-c = 1;
+a = 0.5*f;
+c = 7/f;
 	
 
 A = [0, 1; -k/m, -b/m];
@@ -24,6 +24,7 @@ x = zeros(size(T));
 dx = zeros(size(T));
 ddx = zeros(size(T));
 FS = zeros(size(T));
+M_pos = zeros(size(T));
 M_pos_hat = zeros(size(T));
 M_fs_hat = zeros(size(T));
 M_fuzzy = zeros(size(T));
@@ -34,13 +35,16 @@ for t = T(3:end)
 	F = 0;
 	U(t) = g+F/m;
 	S(:, t) = sysd.A*S(:, t-1) + sysd.B*U(t);
-	x(t) = S(1, t) + 0.0*m*randn();
-	dx(t) = S(2, t) + 0.0*m*randn();
-	ddx(t) = (S(2, t) - S(2, t-1)) + 0.0*m*randn();
+	x(t) = S(1, t) + 0.00009*randn();
+	dx(t) = S(2, t);
+	ddx(t) = (S(2, t) - S(2, t-1));
 	FS(t) = ddx(t)*m + 0.02*m*randn() + m*g;
 	FS_mean = movmean(FS(1:t), 10*f);
 	M_fs_hat(t) = FS_mean(t)/g;
-	M_pos_hat(t) = (-(1/f)^2*k*x(t-2) -b*(1/f)*x(t-1) -b*(1/f)*x(t-2))/(x(t)-2*x(t-1)-x(t-2)-(1/f)*U(t)-(1/f)*U(t-1));
+    Tf = 1/f;
+	M_pos(t) = (-k*(x(t))-b*(x(t)-x(t-1))/Tf)/((x(t)-2*x(t-1)+x(t-2))/Tf/Tf -g);
+    POS_mean = movmean(M_pos(1:t), 10*f);
+    M_pos_hat(t) = POS_mean(t);
 	W(t) = dsigmf(ddx(t), [a, -c, a, c]);
 	M_fuzzy(t) = W(t)*M_fs_hat(t) + (1-W(t))*M_pos_hat(t);
 	M_fuzzy_filter = movmean(M_fuzzy(1:t), 10*f);
@@ -51,19 +55,25 @@ t = length(T);
 Time = 0:1/f:t/f;
 Time = Time(1:end-1);
 
-f = figure(1);
-plot(Time, [x; dx; ddx; FS], '-');
-legend('x', 'dx', 'ddx', 'FS');
+fig = figure(1);
+subplot(2, 1, 1)
+plot(Time, [x; dx; ddx], '-');
+legend('x', 'dx', 'ddx');
 xlabel('t');
 
+subplot(2, 1, 2)
+plot(Time, [FS], '-');
+legend('FS');
+axis([0 inf, -35 -25])
+xlabel('t');
 
-f = figure(2);
+fig = figure(2);
 plot(Time, [M_fs_hat; M_pos_hat; M_fuzzy; W], '-');
 legend('M\_fs\_hat', 'M\_pos\_hat', 'M\_fuzzy', 'W');
 xlabel('t');
 
-f = figure(3);
-w = -10:0.1:10;
+fig = figure(3);
+w = -100/f:0.1/f:100/f;
 plot(w, dsigmf(w, [a, -c, a, c]), '-');
 xlabel('ddx');
 ylabel('W');
